8caf14cbbe1c8cc06b38acc7e1511cd8b1b46388
Bode Falade <bode.falade@outlook.com>
2019-01-17 09:35:54 -0600

    input working.

---
+++ src/components/EnterNumber.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+class EnterNumber extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            inputNumber: 0,
+        };
+    }
+
+        handleChange = (event) => {
+            this.setState({
+                // this will be a string
+                inputNumber: parseInt(event.target.value)
+            })
+        }
+
+    render(){
+
+        return(
+            <div>
+                {JSON.stringify(this.state)}
+                <input onChange={this.handleChange} type="text" />
+                <button>Up</button>
+            </div>
+
+        )
+    }
+}
+
+export default EnterNumber;
